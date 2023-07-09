7e52a748a81552b5548c953c1ba4a2a8e603b1f5
Carlos A. Cabrera <cacabrer@adobe.com>
2023-07-09 14:27:57 -0500

    esm

--- lib/template.ts
+++ lib/template.ts
@@ -1,12 +1,11 @@
 import {
   renderFile as renderEJSFile,
   render as renderEJS,
-} from 'https://deno.land/x/dejs@0.10.3/mod.ts';
-import { marked } from 'https://esm.sh/marked@5.1.1';
-import { markedHighlight } from 'https://esm.sh/marked-highlight@2.0.1';
-import hljs from 'https://esm.sh/highlight.js@11.8.0';
+} from 'https://esm.sh/v128/ejs@3.1.9';
+import { marked } from 'https://esm.sh/v128/marked@5.1.1';
+import { markedHighlight } from 'https://esm.sh/v128/marked-highlight@2.0.1';
+import hljs from 'https://esm.sh/v128/highlight.js@11.8.0';
 import { PAGES_PATH } from './config.ts';
-import { readAll } from 'https://deno.land/std@0.137.0/streams/conversion.ts';
 
 // Check if app template exists
 await Deno.open(`${PAGES_PATH}/template.ejs`).catch(() => {
@@ -45,5 +44,5 @@ export async function renderTemplate(
           __content,
         })
       : __content,
-  }).then(readAll);
+  });
 }
