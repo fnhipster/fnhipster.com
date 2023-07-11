1817375f01aa227eb3ffbc6c951010b213a8a0b7
Carlos A. Cabrera <cacabrer@adobe.com>
2023-07-11 10:17:30 -0500

    url

--- lib/template.ts
+++ lib/template.ts
@@ -2,7 +2,7 @@ import {
   renderFile as renderEJSFile,
   render as renderEJS,
 } from 'https://esm.sh/v128/ejs@3.1.9';
-import { marked } from 'https://esm.sh/v128/marked@5.1.1';
+import { marked } from 'https://deno.land/x/marked@1.0.1/mod.ts';
 import { markedHighlight } from 'https://esm.sh/marked-highlight@2.0.1';
 import hljs from 'https://esm.sh/highlight.js@11.8.0';
 import { PAGES_PATH } from './config.ts';
