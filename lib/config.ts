e38bcdcdd33926b850e82bd792ab44c0f6e249b9
Carlos A. Cabrera <cacabrer@adobe.com>
2023-07-17 17:06:38 -0500

    clear

--- lib/config.ts
+++ lib/config.ts
@@ -1,4 +1,6 @@
 export const PORT = Number(Deno.env.get('PORT')) || 8080;
+export const PAGES_PATH = Deno.cwd() + '/pages';
+export const BUILD_PATH = Deno.cwd() + '/cache';
 
 export interface Meta {
   slug: string;
