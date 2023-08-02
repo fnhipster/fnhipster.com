9183cb125d96d9403d0fb5a48215b445d8441c61
Carlos A. Cabrera <cacabrer@adobe.com>
2023-08-02 13:14:15 -0500

    load fonts async, binary

--- pages/script.js
+++ pages/script.js
@@ -1,1 +1,44 @@
-function init() {}
+function init() {
+  loadAdobeFonts('joe8rol');
+  loadBinaryMessage();
+
+  function loadAdobeFonts(id) {
+    const $link = document.createElement('link');
+    $link.rel = 'stylesheet';
+    $link.href = `https://use.typekit.net/${id}.css`;
+
+    document.head.insertBefore(
+      $link,
+      document.head.childNodes[document.head.childNodes.length - 1].nextSibling
+    );
+  }
+
+  function loadBinaryMessage() {
+    const string = 'True Love Will Find You in the End';
+
+    const binary = string
+      .split('')
+      .map((char) => char.charCodeAt(0).toString(2).padStart(8, '0'));
+
+    binary.push('0x03');
+
+    const $message = document.querySelector('[data-message]');
+
+    let current = Number(localStorage.getItem('message-index')) ?? 0;
+
+    $message.textContent = binary[current];
+
+    const interval = setInterval(() => {
+      $message.textContent = binary[current];
+
+      current++;
+
+      if (current === binary.length) {
+        clearInterval(interval);
+        current = 0;
+      }
+
+      localStorage.setItem('message-index', current);
+    }, 2000);
+  }
+}
